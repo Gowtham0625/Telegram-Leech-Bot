af9ffe72ed8ec2878e9f1b43d1e47995e0f04abd
Abir Hasan <hackitabirhasan@gmail.com>
2020-09-29 20:23:57 +0600

    Add files via upload

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -40,7 +40,7 @@ from tobrot import (
 from pyrogram import Client, filters
 from pyrogram.handlers import MessageHandler, CallbackQueryHandler
 
-from tobrot.plugins.new_join_fn import new_join_f, help_message_f, rename_message_f
+from tobrot.plugins.new_join_fn import new_join_f, welcome_message_f, help_message_f, rename_message_f
 from tobrot.plugins.incoming_message_fn import incoming_message_f, incoming_youtube_dl_f, incoming_purge_message_f, incoming_gdrive_message_f, g_yt_playlist, g_clonee
 from tobrot.plugins.rclone_size import check_size_g, g_clearme
 from tobrot.plugins.status_message_fn import (
@@ -182,7 +182,7 @@ if __name__ == "__main__" :
     app.add_handler(new_join_handler)
     #
     group_new_join_handler = MessageHandler(
-        help_message_f,
+        welcome_message_f,
         filters=filters.chat(chats=AUTH_CHANNEL) & filters.new_chat_members
     )
     app.add_handler(group_new_join_handler)
