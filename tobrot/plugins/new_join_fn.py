6a529fbaa429161a5ff79952ba7d6111932e2d58
Abir Hasan <hackitabirhasan@gmail.com>
2020-09-29 20:13:42 +0600

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -31,13 +31,14 @@ async def new_join_f(client, message):
     # delete all other messages, except for AUTH_CHANNEL
     await message.delete(revoke=True)
 
+
 async def help_message_f(client, message):
     # await message.reply_text("no one gonna help you 🤣🤣🤣🤣", quote=True)
     #channel_id = str(AUTH_CHANNEL)[4:]
     #message_id = 99
     # display the /help
     
-    await message.reply_text("`/leech`: Reply to direct download link, it will upload to telegram.\n \n`/gleech`: Reply to direct link, it will upload to cloud.\n \n`/tleech`: This will mirror the telegram files to cloud.\n \n`/status`: Check current status of bot.\n \n`/leech archive`: Upload torrent and files to telegram in tar.gz format.\n \n`/gleech archive`: compress file/folder and upload to cloud.\n \n`/leech unzip`: This will unzip the ZIP file and upload to telegram.\n \n`/gleech unzip`: This will unzip the ZIP file and upload to cloud.\n \n`/leech unrar`: This will unrar the RAR file and upload to telegram\n \n`/gleech unrar`: This will unrar the RAR file and upload to cloud.\n \n`/leech untar`: This will untar the TAR file and upload to telegram.\n \n`/gleech untar`: This will untar the TAR file and upload to cloud.\n \n`/tleech unzip`: This will unzip the telegram ZIP file and upload to cloud.\n \n`/tleech unrar`: This will unrar the telegram RAR file and upload to cloud.\n \n`/tleech untar`: This will untar the telegram TAR file and upload to cloud.\n \n`/getsize`: This will give you total size of your destination folder in cloud.\n \n`/ytdl`: Reply to any YouTube video link to download the video. \n \n`/ytdl gdrive`: Reply to any YouTube video link to download the video. This will download and upload to cloud.\n \n`/pytdl`: Download videos from youtube playlist link and will upload to telegram.\n \n`/pytdl gdrive`: Download youtube playlist and upload to cloud.\n \n`/renewme`: Clear files from server of @AbirHasan2005.\n")
+    await message.reply_text("""`/leech`: Reply to direct download link, it will upload to telegram.\n \n`/gleech`: Reply to direct link, it will upload to cloud.\n \n`/tleech`: This will mirror the telegram files to cloud.\n \n`/status`: Check current status of bot.\n \n`/leech archive`: Upload torrent and files to telegram in tar.gz format.\n \n`/gleech archive`: compress file/folder and upload to cloud.\n \n`/leech unzip`: This will unzip the ZIP file and upload to telegram.\n \n`/gleech unzip`: This will unzip the ZIP file and upload to cloud.\n \n`/leech unrar`: This will unrar the RAR file and upload to telegram\n \n`/gleech unrar`: This will unrar the RAR file and upload to cloud.\n \n`/leech untar`: This will untar the TAR file and upload to telegram.\n \n`/gleech untar`: This will untar the TAR file and upload to cloud.\n \n`/tleech unzip`: This will unzip the telegram ZIP file and upload to cloud.\n \n`/tleech unrar`: This will unrar the telegram RAR file and upload to cloud.\n \n`/tleech untar`: This will untar the telegram TAR file and upload to cloud.\n \n`/getsize`: This will give you total size of your destination folder in cloud.\n \n`/ytdl`: Reply to any YouTube video link to download the video. \n \n`/ytdl gdrive`: Reply to any YouTube video link to download the video. This will download and upload to cloud.\n \n`/pytdl`: Download videos from youtube playlist link and will upload to telegram.\n \n`/pytdl gdrive`: Download youtube playlist and upload to cloud.\n \n`/renewme`: Clear files from server of @AbirHasan2005.\n""", disable_web_page_preview=True)
 
 
 async def rename_message_f(client, message):
@@ -50,7 +51,7 @@ async def rename_message_f(client, message):
     ])
     reply_markup = pyrogram.InlineKeyboardMarkup(inline_keyboard)
     await message.reply_text(
-        "Please use @renamebot",
+        "please use @renamebot",
         quote=True,
         reply_markup=reply_markup
     )
